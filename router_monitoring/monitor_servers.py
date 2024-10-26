782f53de551579842daf7440468b478a47007c2c
Jyoshna Kotha <147752827+hfcbhjjfk@users.noreply.github.com>
2024-10-25 18:27:41 -0700

    Update monitor_servers.py

--- router_monitoring/monitor_servers.py
+++ router_monitoring/monitor_servers.py
@@ -46,7 +46,7 @@ def monitor_server(ip, instance_id, private_key):
 
 # Collect metrics from all servers
 servers = [
-    {"ip": "54.172.223.186", "id": "First_server", "key": "/home/Documents/first_keypair.pem"},
+    {"ip": "54.172.223.186", "id": "First_server", "key": "/absolute/path/to/first_keypair.pem"},
     # Add more servers here
 ]
 
